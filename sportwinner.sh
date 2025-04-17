78581839bbbc55e8e91de1c593767b6687599f9b
Hohnik <mail@niklashohn.com>
2025-04-17 21:23:59 +0200

    Cleanup

--- sportwinner.sh
+++
@@ -1,6 +0,0 @@
-curl 'https://bskv.sportwinner.de/php/bskv/service.php' \
-  -H 'Referer: https://bskv.sportwinner.de/' \
-  --data-raw 'command=GetSpielerInfo&id_saison=2&id_spiel=8107' \
-  # --compressed \
-  # --output service.php
-

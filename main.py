b71a707e765158df8d22bddc6656fb3c216a99cd
Hohnik <mail@niklashohn.com>
2025-04-17 21:16:13 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,94 @@
+import json
+from enum import Enum
+from requests import post
+
+
+def main():
+    for saison_id, saison in get_saisonen().items():
+        print()
+        print(f"Saison-{saison_id}: {saison}")
+        for liga_id, liga in get_ligen(saison_id).items():
+            print(f"Liga-{liga_id}: {liga}")
+            for spiel_id, spiel in get_spiele(
+                saison_id=saison_id, liga_id=liga_id
+            ).items():
+                print(f"Spiel-{spiel_id}: {spiel}")
+            return
+
+    # spiel_ids = get_spiele(2, 1, 1)
+    # get_spiel(spiel)
+
+
+def get_saisonen():
+    saisonen = {
+        int(year[0]): year[1] for year in sportwinner_api({"command": "GetSaisonArray"})
+    }
+    return saisonen
+
+
+def get_ligen(saison_id, bezirk_id=0, kreis_id=0) -> dict:
+    ligen = {
+        int(liga[0]): liga[1]
+        for liga in sportwinner_api(
+            {
+                "command": "GetLigaArray",
+                "id_saison": saison_id,
+                "id_bezirk": bezirk_id,
+                "id_kreis": kreis_id,
+                "favorit": "",
+                "art": 1,
+            }
+        )
+    }
+    return ligen
+
+
+def get_spiele(saison_id, liga_id, klub_id=0, bezirk_id=0, kreis_id=0, spieltag_id=0):
+    """
+    id, datum, uhrzeit, verein,
+    """
+    spiele_data = {
+        "command": "GetSpiel",
+        "id_saison": saison_id,
+        "id_klub": klub_id,
+        "id_bezirk": bezirk_id,
+        "id_kreis": kreis_id,
+        "id_liga": liga_id,
+        "id_spieltag": spieltag_id,
+        "favorit": "",
+        "art_bezirk": 0,
+        "art_kreis": 0,
+        "art_liga": 0,
+        "art_spieltag": 0,
+    }
+
+    spiele = {}
+    for spiel in sportwinner_api(spiele_data):
+        spiele.update(
+            {spiel[0]: {"datum": spiel[1], "heim": spiel[3], "auswaerts": spiel[6]}}
+        )
+    return spiele
+
+
+def get_spiel(saison_id, spiel_id):
+    spiel_data = {
+        "command": "GetSpielerInfo",
+        "id_saison": saison_id,
+        "id_spiel": spiel_id,
+    }
+    body = sportwinner_api(spiel_data)
+    return body
+
+
+def sportwinner_api(data: dict):
+    header = {"Referer": "https://bskv.sportwinner.de/"}
+    response = post(
+        "https://bskv.sportwinner.de/php/bskv/service.php",
+        data=data,
+        headers=header,
+    )
+    return response.json()
+
+
+if __name__ == "__main__":
+    main()
